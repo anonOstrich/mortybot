6ca8a9ca030a330aed30e7ab69d0286fb8501397
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2020-02-01 15:51:03 +0200

    Final changes

--- functions/index.js
+++ functions/index.js
@@ -8,39 +8,51 @@ const {WebhookClient} = require('dialogflow-fulfillment');
 
 
 
+
+
 const movieDetails =  {
   titanic: {
     director: '',
     year: ''
   },
-  twilight: "Kuka lie"
+  twilight: {
+    director: '',
+    year: ''
+  },
+  toinen: {
+    director: '',
+    year: ''
+  }
 }
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
-
-
-const welcome = (agent)  => {
-  agent.add(`Test webhook!`);
-}
 
 const handleDirector = (agent) => {
-  const year = 
+  const context = agent.getContext("movie-has-been-chosen")
+  const movie =  ''// context.parameters.movie, or however the entity value can be fetched
+  const year = movieDetails(movie).year
+  agent.add(`The movie ${movie} was made in ${year}`)
 }
 
 
+const handleYear = (agent) => {
 
+  const context = agent.getContext("movie-has-been-chosen")
+
+  const movie =  ''// context.parameters.movie, or however the entity value can be fetched
+
+  const director = movieDetails(movie).director
+  agent.add(`The movie ${movie} was directed by ${director}`)
+}
 
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({ request, response });
   let intentMap = new Map();
   // ennen boilerplate
-  intentMap.set('Roska', welcome);
+
   intentMap.set('Director', handleDirector);
   intentMap.set('What year was it made?', handleYear)
-  
+
   // jälkeen boilerplate
   agent.handleRequest(intentMap);
   });
