87683334adf05d565b8dcf4aa250b154b75e0147
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2020-02-01 14:55:23 +0200

    Add function to handle generic fulfillment

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Morty
 
+## Firebase?
+
+[https://firebase.google.com/docs/functions/get-started?authuser=0](https://firebase.google.com/docs/functions/get-started?authuser=0)
+
 ## Configuration
 
 'Hello world' function available at [https://us-central1-movieagent-rxdtej.cloudfunctions.net/helloWorld](https://us-central1-movieagent-rxdtej.cloudfunctions.net/helloWorld). 
 
 So functions could be: https://us-central1-movieagent-rxdtej.cloudfunctions.net/<name-of-functions>
+
+## Deployment
+
+Make sure you have installed firebase-tools (`npm install -g firebase-tools`, for instance). Then `firebase login`. Deployment is done with `firebase deploy --only functions`. 
--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,24 @@ const functions = require('firebase-functions');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
+const {WebhookClient} = require('dialogflow-fulfillment');
+
+
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
+
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+
+  let response = ""
+  switch(agent.intent){
+    case 'roska':
+      agent.addResponse_("Testiresponse")
+    default: 
+      agent.addResponse_("Hämmennysresponse")
+      break;
+  }
+  });
